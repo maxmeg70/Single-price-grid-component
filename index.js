aa2b6614de2f0039bca359627511ab6741ae60e2
maxmeg70 <massimo.indelicato@gmail.com>
2023-09-05 13:05:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
 const btnSignUp = document.querySelector('#sign-up');
-const myElement = document.querySelector('#myElement');
-const myValue=document.querySelector('#myValue');
-myValue.innerHTML='Sign Up';
 
-btnSignUp.addEventListener('click',()=>{
+btnSignUp.addEventListener('click', ()=>{
     setTimeout(()=>{
-        btnSignUp.classList.add('button-transparent');
-        myValue.innerHTML="Thanks !"
+        btnSignUp.classList.add('button-sign');
         btnSignUp.disabled=true;
-           
-    },800);
-    let myP = document.createElement('p');
-        myP.textContent='Please Wait !';
-        myP.setAttribute('class','text-white text-center fs-4');
-        myElement.appendChild(myP);
-        clearAll(myP);
+        btnSignUp.style.display='none';
+        alert('If you click ok the button will disappear, Thanks for play');
+        clearAll();
         
-});
-function clearAll(element){
-   
-    setTimeout(()=>{
         
-        element.innerHTML='';
-        btnSignUp.classList.remove('button-transparent');
-        alert('Many Thanks, please close the window !');
-        location.reload();
-        
-    },5000);
+    },1000);
+    function clearAll(){
+        setTimeout(()=>{
+            confirm('Do you want to play again ?');
+            location.reload();
+        },2500);
+    }
     
-}
+}) 
